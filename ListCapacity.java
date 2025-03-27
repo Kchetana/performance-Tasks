c7f6100fe2088b537268eb4efc16ff54740f1213
konepalli_chetana <konepalli_chetana@epam.com>
2025-03-27 14:04:14 +0530

    capacityTask

---
+++ ListCapacity.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class ListCapacity {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>(10);
+        list.add("Chetana");
+        list.add("ChetanaKonepalli");
+        list.add("ChetanaReddy");
+
+        list.trimToSize();
+
+        System.out.println(list.size());
+    }
+}
